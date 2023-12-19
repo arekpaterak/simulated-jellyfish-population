7c86fd78f331fe02d86fd97852340c444d73fd10
AsgardianVoyager <86044128+arekpaterak@users.noreply.github.com>
2023-12-19 16:06:41 +0100

    Fix id

--- src/model.py
+++ src/model.py
@@ -13,31 +13,24 @@ class MarineEcosystem(mesa.Model):
         self,
         width: int = 20,
         height: int = 20,
-
         initial_jellyfish_medusa_population_size: int = 1000,
         initial_jellyfish_polyp_population_size: int = 100,
         initial_jellyfish_larva_population_size: int = 100,
         initial_sea_turtle_population_size: int = 100,
         initial_fish_population_size: int = 1000,
-
         jellyfish_medusa_time_to_grow: int = 15,
         jellyfish_medusa_gain_from_food: int = 20,
         jellyfish_medusa_reproduction_probability: float = 0.05,
-        jellyfish_medusa_reproduction_rate: int = 5, # larvae per day
-
+        jellyfish_medusa_reproduction_rate: int = 5,  # larvae per day
         jellyfish_polyp_time_to_grow: int = 30,
         jellyfish_polyp_gain_from_food: int = 20,
-        jellyfish_polyp_strobilation_rate: int = 1, # medusae per day
-
+        jellyfish_polyp_strobilation_rate: int = 1,  # medusae per day
         jellyfish_larva_time_to_grow: int = 5,
         jellyfish_larva_gain_from_food: int = 20,
-
         fish_time_to_grow: int = 90,  # days
         fish_gain_from_food: int = 20,
         fish_reproduction_probability: float = 0.05,
-
         sea_turtle_gain_from_food: int = 20,
-
         plantkton_time_to_grow: int = 50
         # TODO: add parameters
     ) -> None:
@@ -72,13 +65,13 @@ class MarineEcosystem(mesa.Model):
         """
         Create a new population of agents of a given type.
         """
-        for i in range(size):
+        for _ in range(size):
             x = self.random.randrange(100)
             y = self.random.randrange(100)
 
             energy = None  # TODO: assign energy
 
-            agent = agent_type(i, (x, y), self)
+            agent = agent_type(self.next_id(), (x, y), self)
 
             self.grid.place_agent(agent, (x, y))
             self.schedule.add(agent)
