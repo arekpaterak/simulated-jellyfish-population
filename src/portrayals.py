3f1b557dc874a0e936a7cdc32338415b0cb88f0f
AsgardianVoyager <86044128+arekpaterak@users.noreply.github.com>
2023-12-19 16:29:17 +0100

    Update portrayals.py

--- src/portrayals.py
+++ src/portrayals.py
@@ -3,67 +3,68 @@ from agents import (
     JellyfishPolyp,
     JellyfishMedusa,
     Plankton,
+    SeaTurtle,
     Fish,
-    SeaTurtle
 )
 
+
 def agent_portrayal(agent):
+    match agent:
+        case JellyfishLarva():
+            portrayal = {
+                "Shape": "circle",
+                "Color": "#f76fa3",
+                "Filled": "true",
+                "Layer": 0,
+                "r": 0.5,
+            }
 
-    if isinstance(agent, JellyfishLarva):
-        portrayal = {
-            "Shape": "circle",
-            "Color": "blue",
-            "Filled": "true",
-            "Layer": 0,
-            "r": 1
-        }
+        case JellyfishPolyp():
+            portrayal = {
+                "Shape": "circle",
+                "Color": "#f76fa3",
+                "Filled": "false",
+                "Layer": 0,
+                "r": 1,
+            }
 
-    if isinstance(agent, JellyfishPolyp):
-        portrayal = {
-            "Shape": "circle",
-            "Color": "blue",
-            "Filled": "false",
-            "Layer": 0,
-            "r": 0.5
-        }
+        # if isinstance(agent, JellyfishMedusa):
+        #     portrayal = {
+        #         "Shape": "circle",
+        #         "Color": "blue",
+        #         "Filled": "true",
+        #         "Layer": 0,
+        #         "r": 0.6
+        #     }
 
-    if isinstance(agent, JellyfishMedusa):
-        portrayal = {
-            "Shape": "circle",
-            "Color": "blue",
-            "Filled": "true",
-            "Layer": 0,
-            "r": 0.6
-        }
+        # if isinstance(agent, Plankton):
+        #     portrayal = {
+        #         "Shape": "rectangle",
+        #         "Color": "green",
+        #         "Filled": "false",
+        #         "Layer": 0,
+        #         "r": 0.4
+        #     }
 
-    if isinstance(agent, Plankton):
-        portrayal = {
-            "Shape": "rectangle",
-            "Color": "green",
-            "Filled": "false",
-            "Layer": 0,
-            "r": 0.4
-        }
+        # if isinstance(agent, SeaTurtle):
+        #     portrayal = {
+        #         "Shape": "circle",
+        #         "Color": "yellow",
+        #         "Filled": "true",
+        #         "Layer": 0,
+        #         "r": 0.6
+        #     }
 
-    if isinstance(agent, SeaTurtle):
-        portrayal = {
-            "Shape": "circle",
-            "Color": "yellow",
-            "Filled": "true",
-            "Layer": 0,
-            "r": 0.6
-        }
+        # if isinstance(agent, Fish):
+        #     portrayal = {
+        #         "Shape": "circle",
+        #         "Color": "purple",
+        #         "Filled": "true",
+        #         "Layer": 0,
+        #         "r": 0.4
+        #     }
 
-    if isinstance(agent, Fish):
-        portrayal = {
-            "Shape": "circle",
-            "Color": "purple",
-            "Filled": "true",
-            "Layer": 0,
-            "r": 0.4
-        }
-
-    else:
-        portrayal = {}
+        case _:
+            portrayal = {}
 
     return portrayal
